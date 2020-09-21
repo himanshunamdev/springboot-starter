05fcf0e7e111c036589c477c6a4b8747a0626dc2
Himanshu Namdeo <himanshu.namdeo6@gmail.com>
2020-09-21 16:42:42 +0530

    First commit : Spring-boot starter

---
+++ src/test/java/com/himi/springboot/SpringbootSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
